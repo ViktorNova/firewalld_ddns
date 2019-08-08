ba252c92493916d68e8fe18ef458de5fd0330ac2
Viktor Nova <ViktorNova@users.noreply.github.com>
2019-08-08 16:22:44 -0700

    Update update_trusted_source.sh

--- update_trusted_source.sh
+++ update_trusted_source.sh
@@ -25,12 +25,12 @@ fi
 function fn_update ()
 {
 OLDIP=$(/bin/cat ./ip_of_$dyn_name 2>/dev/null)
-if [[ $NEWIP == $OLDIP ]];
+if [ "$NEWIP" == "$OLDIP" ];
  then
   echo "IP has not changed - exiting"
   exit 2
  else
-  echo "New IP address found for $1 - $NEWIP"
+  echo "New IP address found for $DYN_NAME - $NEWIP"
   fn_update_action
 fi
 }

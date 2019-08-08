e71e7558b2dd200df7e2111ec4531db317d5190e
Viktor Nova <ViktorNova@users.noreply.github.com>
2019-08-08 15:09:54 -0700

    Adapt it to work with custom firewalld rules
    using firewalld xml templates

--- update_trusted_source.sh
+++ update_trusted_source.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 cd $(dirname $0)
 
+# The zone file with the custom rule you want to update
+ZONEFILE="/etc/firewalld/zones/public.xml"
+# This template needs to be created in advance and have the IP address replaced with DDNS_IPADDRESS
+ZONEFILE_TEMPLATE="/etc/firewalld/zones/public.xml.TEMPLATE"
+
 # Using positional vars $1 domain name $2 zone
 dyn_name="$1"
 # defaults to trusted zone
 if [[ $2 = "" ]];
  then
-  zone="trusted"
+  zone="public"
  else
   zone="$2"
 fi
@@ -23,45 +28,31 @@ if [[ $newip == "" ]];
   exit 2
 fi
 
-# check if new ip present in firewall. If present in desired zone - exit.
-# If present in different zone - exit with error
-function fn_in_firewall ()
-{
-if [[ $(firewall-cmd --get-active-zones) =~ $newip ]];
- then
-  if [[ ! $(firewall-cmd --list-sources --zone=$zone) =~ $newip ]];
-   then
-    logger update_trusted_source.sh:error:source conflict domain=$dyn_name zone=$zone ip=$newip
-    exit 2
-   else 
-    exit
-  fi
-fi
-}
 
-# check if old ip in firewall. Update firewall
+# Check if old ip is different than new ip
 function fn_update ()
 {
 oldip=$(/bin/cat ./ip_of_$dyn_name 2>/dev/null)
-if [[ $(firewall-cmd --list-sources --zone=$zone) =~ $oldip ]] && [[ ! $oldip == "" ]];
+if [[ $newip =~ $oldip ]];
  then
-  firewall-cmd --remove-source=$oldip --zone=$zone
-  logger update_trusted_source.sh:info:rule changed ip removed domain=$dyn_name zone=$zone oldip=$oldip
-  fn_update_action
+  echo "IP has not changed - exiting"
+  exit 2
  else
   fn_update_action
 fi
 }
 
-# for compact code
+# Copy a firewalld template over the active one and swap out the IP address.
 function fn_update_action ()
 {
-  firewall-cmd --add-source=$newip --zone=$zone
+  cp $ZONEFILE $ZONEFILE_TEMPLATE
+  sed -i 's/DDNS_IPADDRESS/'DDNS_IPADDRESS'/g' /etc/$ZONEFILE
   echo $newip > ./ip_of_$dyn_name
+  systemctl restart firewalld
   logger update_trusted_source.sh:info:rule changed ip added domain=$dyn_name zone=$zone newip=$newip
+  
 }
 
 fn_dig_check
-fn_in_firewall
 fn_update
 exit
